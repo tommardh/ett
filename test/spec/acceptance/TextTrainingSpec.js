f3b8519ac3ac108158d91c04860d8da7d4f878fc
Tom Mårdh <tom.mardh@softhouse.se>
2015-03-07 10:54:28 +0100

    Initial version

---
+++ test/spec/acceptance/TextTrainingSpec.js
@@ -0,0 +1,18 @@
+describe("Text Training", function() {
+  
+  beforeEach(function() {
+    
+  });
+
+it("xxx", function() {
+    //GIVEN
+    //WHEN
+    //THEN
+    var thisResult = {state : "Not Implemented"};
+    var expectedResult = {};
+    expect(thisResult).toEqual(expectedResult);
+  });
+
+});
+
+ 
